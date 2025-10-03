6fea6d5ef48f2bf5d05a51c8887aef5df102f7a1
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2025-10-03 16:46:42 +0200

    sumAll complete

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  
+  let sum = 0;
+  for (let index = min; index <= max; index++) {
+    sum += index;
+  }
+  return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
