f72996a2e0454078fbb17f577bac7654b0ff0043
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2025-10-03 17:48:29 +0200

    leapYears complete

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(anno) {
+  if (anno % 4 != 0) return false;
+  if (anno % 100 != 0) return true;
+  if (anno % 400 != 0) return false;
+  return true;
 };
 
 // Do not edit below this line
